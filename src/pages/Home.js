be729f50b81e2da29a5be3e0a3302f9a4386ca27
Vibodh Bhosure <vibodhbhoure@gmail.com>
2022-12-18 16:55:29 +0530

    Prediction Output Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,8 @@ function Home() {
         message: "",
         prediction: ""
     });
+    const [submitButtonText, setsubmitButtonText] = useState("Predict");
+    const [submitButton, setsubmitButton] = useState(false);
     const [q1, setq1] = useState('');
     const [q2, setq2] = useState('');
     const [q3, setq3] = useState('');
@@ -69,6 +71,7 @@ function Home() {
     };
     const handleChangefamilyjaundice = (event) => {
         setfamilyjaundice(event.target.value);
+        setsubmitButton(true);
     };
     const handleChangealgo = (event) => {
         setalgo(event.target.value);
@@ -82,15 +85,38 @@ function Home() {
     let baseUrl = `https://autism-detection-system.onrender.com/api/algo/${algo}/data/${answer}`;
 
     async function identifyChange() {
+        setsubmitButton(false);
+        setsubmitButtonText("Predicting...");
         await axios.get(baseUrl).then((response) => {
             setdata(response.data);
         });
+        document.getElementById("hidden").style.display = "block";
+        setsubmitButton(true)
+        setsubmitButtonText("Predict");
+    }
+
+    function closeOverlay() {
+        document.getElementById("hidden").style.display = "none";
     }
 
     console.log(data);
 
     return (
         <div>
+            <div id="hidden">
+                <div className="container-fluid tex-center  ">
+                    <div className="row" style={{ marginTop: "15.688rem" }}>
+                        <div className="col-md">
+                            <p className="label-text" style={{ color: "#3377ff", textShadow: "2px 0 #fff, -2px 0 #fff, 0 2px #fff, 0 -2px #fff,1px 1px #fff, -1px -1px #fff, 1px -1px #fff, -1px 1px #fff" }}>The Prediction is: {data.message} </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md">
+                            <button className="btn btn-warning" onClick={closeOverlay}>Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="jumbotron homeapp tex-center">
                 <div>
                     <h1 id="title1">Autism Screening Test</h1>
@@ -102,7 +128,7 @@ function Home() {
                                 <p>Does your child look at you when you call his/her name?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq1}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"}>Always</option>
                                     <option value={"0"}>Usually</option>
                                     <option value={"1"}>Sometimes</option>
@@ -116,7 +142,7 @@ function Home() {
                                 <p>How easy is it for you to get eye-contact with your child?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq2}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"} >Always</option>
                                     <option value={"0"}>Usually</option>
                                     <option value={"1"}>Sometimes</option>
@@ -132,7 +158,7 @@ function Home() {
                                 <p>Does your child point to indicate that he/she wants something?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq3} >
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"} >Always</option>
                                     <option value={"0"}>Usually</option>
                                     <option value={"1"}>Sometimes</option>
@@ -146,7 +172,7 @@ function Home() {
                                 <p>Does your child point to share interest with you?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq4}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"} >Always</option>
                                     <option value={"0"}>Usually</option>
                                     <option value={"1"}>Sometimes</option>
@@ -162,7 +188,7 @@ function Home() {
                                 <p>Does your child pretend?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq5}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"} >Always</option>
                                     <option value={"0"}>Usually</option>
                                     <option value={"1"}>Sometimes</option>
@@ -176,7 +202,7 @@ function Home() {
                                 <p>Does your child follow where you are looking?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq6}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"} >Always</option>
                                     <option value={"0"}>Usually</option>
                                     <option value={"1"}>Sometimes</option>
@@ -192,7 +218,7 @@ function Home() {
                                 <p>If you or someone in your family is visibly upset,</p>
                                 <p>does your child show signs of wanting to comfort them?</p>
                                 <select className="form-control" onChange={handleChangeq7}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"}>Always</option>
                                     <option value={"0"}>Usually</option>
                                     <option value={"1"}>Sometimes</option>
@@ -206,7 +232,7 @@ function Home() {
                                 <p>Would you describe your child first words as?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq8} >
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"}>Very Typical</option>
                                     <option value={"0"}>Quite Typical</option>
                                     <option value={"1"}>Slightly Unusual</option>
@@ -222,7 +248,7 @@ function Home() {
                                 <p>Does your child use simple gestures?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq9}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"}>Always</option>
                                     <option value={"0"}>Usually</option>
                                     <option value={"1"}>Sometimes</option>
@@ -236,7 +262,7 @@ function Home() {
                                 <p>Does your child stare at nothing with no apparent purpose?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeq10}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"1"} >Always</option>
                                     <option value={"1"}>Usually</option>
                                     <option value={"0"}>Sometimes</option>
@@ -260,7 +286,7 @@ function Home() {
                                 <p>Enter Child sex:</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangesex}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"1"}>Male</option>
                                     <option value={"0"}>Female</option>
                                 </select>
@@ -273,7 +299,7 @@ function Home() {
                                 <p>Enter child Ethnicity:</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangeethnicity}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"0"}>Hispanic</option>
                                     <option value={"1"}>Latino</option>
                                     <option value={"3"}>Others</option>
@@ -292,7 +318,7 @@ function Home() {
                                 <p>Does the child has an history of jaundice?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangechildjaundice}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"1"}>Yes</option>
                                     <option value={"0"}>No</option>
                                 </select>
@@ -305,7 +331,7 @@ function Home() {
                                 <p>Does the child family has an ASD history?</p>
                                 <p>&nbsp;</p>
                                 <select className="form-control" onChange={handleChangefamilyjaundice}>
-                                    <option value={"0"}>Select a option...</option>
+                                    <option value={""}>Select a option...</option>
                                     <option value={"1"}>Yes</option>
                                     <option value={"0"}>No</option>
                                 </select>
@@ -324,7 +350,7 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <button type="button" className="btn btn-success" onClick={identifyChange}>Predict</button>
+                <button type="button" className="btn btn-success" onClick={identifyChange} disabled={!submitButton}>{submitButtonText}</button>
             </div >
         </div>
     );
